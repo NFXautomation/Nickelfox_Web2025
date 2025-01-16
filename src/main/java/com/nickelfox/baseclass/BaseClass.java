d3eee336e6d6c76bbfddbec1691cbc567316af5d
NFXautomation <avinash@nickelfox.com>
2025-01-16 19:12:17 +0530

    Update BaseClass.java
    update

--- src/main/java/com/nickelfox/baseclass/BaseClass.java
+++ src/main/java/com/nickelfox/baseclass/BaseClass.java
@@ -3,6 +3,7 @@ package com.nickelfox.baseclass;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
@@ -10,7 +11,8 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.ITestResult;
 
 import org.testng.annotations.AfterMethod;
@@ -22,25 +24,28 @@ public class BaseClass {
 
 	public WebDriver driver;
 	//@BeforeMethod()
-	public void launcTheBrowser() throws InterruptedException {
+	public void launcTheBrowser() throws InterruptedException  {
 
 		//WebDriverManager.firefoxdriver().setup();
 		WebDriverManager.chromedriver().setup();
 
-
-		//FirefoxOptions c = new FirefoxOptions();
-		ChromeOptions c = new ChromeOptions();
-		c.addArguments("--incognito");
-		//c.addArguments("--disable-notifications");
-		c.addArguments("--remote-allow-origins=*");
-		c.addArguments("start-maximized"); // open Browser in maximized mode
-		//	c.addArguments("--disable-infobars"); // disabling infobars
-	//	c.addArguments("--headless");
-		driver = new ChromeDriver(c);
-		//driver = new FirefoxDriver(c);
-		driver.navigate().to("https://www.nickelfox.com/");
-		//Thread.sleep(8000);
-
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("start-maximized"); // open Browser in maximized mode
+		options.addArguments("--disable-infobars"); // disabling infobars
+		options.addArguments("--disable-extensions"); // disabling extensions
+		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+		options.addArguments("--no-sandbox");// Bypass OS security model
+		options.addArguments("--disable-gpu"); // applicable to windows os only
+		options.addArguments("--headless");
+		options.addArguments("--window-position=1920,1080");
+		options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36");
+	
+		driver = new ChromeDriver(options=options);
+		driver.manage().window().maximize();
+		driver.get("https://nickelfox.com/");
+		//driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		String strTitle=driver.getTitle();
+		System.out.println("Title =" + " " + strTitle);
 		System.out.println("user navigate to the website");
 
 
